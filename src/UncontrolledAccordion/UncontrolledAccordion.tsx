8ef86d782d9f4d82e813b8e560dad985715011ac
fanur56 <ilasovf@gmail.com>
2022-08-07 21:47:01 +0300

    Refactor UncontrolledAccordion.tsx with useReducer

--- src/UncontrolledAccordion/UncontrolledAccordion.tsx
+++ src/UncontrolledAccordion/UncontrolledAccordion.tsx
@@ -1,29 +1,44 @@
-import React, {useState} from "react";
+import React, {useReducer} from "react";
 
 type AccordionType = {
     titleValue: string
 }
 
+type ActionType = {
+    type: string
+}
+
+const reducer = (state: boolean, action: ActionType) => {
+    if (action.type === "TOGGLE-COLLAPSED") {
+        return !state;
+    }
+    return state;
+}
+
 export const UncontrolledAccordion = (props: AccordionType) => {
-    let [collapsed, setCollapsed] = useState(false)
+
+    let [collapsed, dispatch] = useReducer(reducer, false)
+
     return (
         <div>
             <AccordionTitle title={props.titleValue}
-                            onClick={() => {setCollapsed(!collapsed)}} />
-            {collapsed && <AccordionBody/>}
+                            onClick={() => {dispatch({type: "TOGGLE-COLLAPSED"})}}/>
+            {!collapsed && <AccordionBody/>}
         </div>
     );
 }
 
 type AccordionTitleType = {
     title: string
-    onClick:()=>void
+    onClick: () => void
 }
 
 function AccordionTitle(props: AccordionTitleType) {
 
     return (
-        <h3 onClick={()=>{props.onClick()}}>{props.title}</h3>
+        <h3 onClick={() => {
+            props.onClick()
+        }}>{props.title}</h3>
     );
 }
 
