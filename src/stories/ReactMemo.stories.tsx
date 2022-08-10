0956bee9087c790c8b4df448f5b47804b6c91ff0
fanur56 <ilasovf@gmail.com>
2022-08-10 20:08:51 +0300

    Created ReactMemo.stories.tsx for exercise with react.memo

---
+++ src/stories/ReactMemo.stories.tsx
@@ -0,0 +1,35 @@
+import React, {useState} from "react";
+
+export default {
+    title: "react memo demo"
+}
+
+const NewMessageCounter = (props: { count: number }) => {
+    return (
+        <div>{props.count}</div>
+    )
+}
+
+const UsersSecret = (props: { users: Array<string> }) => {
+    console.log("render usersSecret component")
+    return (
+        <div>{props.users.map((u, index) => <div key={index}>{u}</div>)}</div>
+    )
+}
+
+const Users = React.memo(UsersSecret)
+
+export const Example1 = () => {
+    const [count, setCount] = useState(0)
+    const [users, setUsers] = useState(["Igor", "Victor", "Dima"])
+    const addUser = ()=>{
+        setUsers([...users, "Sveta" + new Date().getTime()])
+    }
+
+    return <>
+        <button onClick={() => setCount(count + 1)}>+</button>
+        <button onClick={addUser}>Add user</button>
+        <NewMessageCounter count={count}/>
+        <Users users={users}/>
+    </>
+}
